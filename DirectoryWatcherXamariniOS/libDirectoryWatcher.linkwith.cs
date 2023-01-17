cda56acd1f8e874b15e5e81e20426c0772b4c0be
Виктор Кочетков <victor.development@yandex.ru>
2023-01-17 12:00:27 +0300

    Revert "---"
    This reverts commit 78779a6ad7e37568bba242b989b3082e1eb85e30.

--- DirectoryWatcherXamariniOS/libDirectoryWatcher.linkwith.cs
+++ DirectoryWatcherXamariniOS/libDirectoryWatcher.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith("libDirectoryWatcher.a", LinkTarget.Arm64 | LinkTarget.Simulator64, ForceLoad = true, SmartLink = true)]
+[assembly: LinkWith ("libDirectoryWatcher.a", LinkTarget.Simulator64 | LinkTarget.Arm64, ForceLoad = true, SmartLink=true)]
